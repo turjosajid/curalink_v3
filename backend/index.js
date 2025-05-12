c541e82aaefbc63e35323d6d80d562180aec751e
turjosajid <sajid.turjo@gmail.com>
2025-05-12 09:40:18 +0600

    fix: mongodb error

--- backend/index.js
+++ backend/index.js
@@ -31,10 +31,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
     // Start server after successful MongoDB connection
